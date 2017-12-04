c7491746373e63f6e2b3804dd7719022fcaf77b3
Kevin Hannay <khannay@schreiner.edu>
2017-12-04 13:33:32 -0600

    added a grab Numeric function for cluster analysis

---
+++ R/grabNumeric.R
@@ -0,0 +1,20 @@
+#' Grab Numeric
+#'
+#' This function returns a new data frame with only the numeric columns for the original data frame. This is especially useful for cluster analysis.
+#'
+#' @param mydf this is the data frame to strip of non numerical columns
+#' @param drop defaults to NULL (none) use this if you want to drop one of more columns by name. Non numerical columns will be dropped automatically
+#' @return a data frame with all numerical columns
+#' @examples
+#' data("Bensonetal")
+#' b2<-grabNumeric(Bensonetal) # removes non numerical columns
+#' @export
+
+
+
+grabNumeric <- function(mydf, drop=NULL) {
+    mydf<-mydf[ , !(names(mydf) %in% drop)]
+    nums<-sapply(mydf, is.numeric)
+    ret.value<-mydf[,nums]
+    return(ret.value)
+}
