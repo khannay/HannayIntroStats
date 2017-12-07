017921402f84b5923ff99eed177cf62f47ccbf2b
Kevin Hannay <khannay@schreiner.edu>
2017-12-07 16:00:51 -0600

    grabNumeric function updated

--- R/grabNumeric.R
+++ R/grabNumeric.R
@@ -4,6 +4,7 @@
 #'
 #' @param mydf this is the data frame to strip of non numerical columns
 #' @param drop defaults to NULL (none) use this if you want to drop one of more columns by name. Non numerical columns will be dropped automatically
+#' @param drop.na.values defaults to TRUE, this will drop any rows which contain missing values. This is done AFTER removing the drop columns AND dropping non numerical columns
 #' @return a data frame with all numerical columns
 #' @examples
 #' data("Bensonetal")
@@ -12,9 +13,12 @@
 
 
 
-grabNumeric <- function(mydf, drop=NULL) {
+grabNumeric <- function(mydf, drop=NULL, drop.na.values=TRUE) {
     mydf<-mydf[ , !(names(mydf) %in% drop)]
     nums<-sapply(mydf, is.numeric)
     ret.value<-mydf[,nums]
+    if (drop.na.values==TRUE) {
+      ret.value<-na.omit(ret.value)
+    }
     return(ret.value)
 }
