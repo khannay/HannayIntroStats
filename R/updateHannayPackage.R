7d322876feabc86f0b194fe42d7dfbcca99b616f
Kevin Hannay <khannay@umich.edu>
2017-12-09 09:35:41 -0600

    update package

---
+++ R/updateHannayPackage.R
@@ -0,0 +1,10 @@
+#' Update the Hannay Package
+#'
+#' No arguments, does not return anything
+#'
+#' @export
+
+updateHannayPackage <- function() {
+devtools::install_bitbucket('khannay/hannayintrostats.git')
+
+}
