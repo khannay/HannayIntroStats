731d477e1c8e067fafea789b90c0654a92c341a5
Kevin Hannay <khannay@schreiner.edu>
2018-10-31 13:53:47 -0500

    letter grade convert function added

---
+++ R/letter.grade.convert.R
@@ -0,0 +1,13 @@
+
+
+
+
+letter.grade.convert<-function(num_grades) {
+  
+  ifelse(num_grades >=90, "A",
+         ifelse(num_grades >=80, "B",
+                ifelse(num_grades >=70, "C", 
+                       ifelse(num_grades, "D", "F"))))
+  
+  
+}
